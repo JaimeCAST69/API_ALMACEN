a8457cde0a9d64b71cc28d5e673568042e79e384
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-07-01 18:52:24 -0600

    [UPDATE] implementacion de jwt

--- src/main/java/utez/edu/mx/unidad3/security/MainSecurity.java
+++ src/main/java/utez/edu/mx/unidad3/security/MainSecurity.java
@@ -2,8 +2,15 @@ package utez.edu.mx.unidad3.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,8 +25,8 @@ public class MainSecurity {
     public SecurityFilterChain doFilterInternal(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable()).cors(c-> c.configurationSource(corsRegistry()))
                 .authorizeHttpRequests(auth ->auth
-                        .requestMatchers("/api/client/**").permitAll()
-                        .requestMatchers("/api/cedes/**").permitAll()
+                        .requestMatchers("/api/client/**").hasRole("ADMIN")
+                        .requestMatchers("/api/cedes/**").hasRole("EMPLOYEE")
                         .requestMatchers(
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
@@ -43,5 +50,34 @@ public class MainSecurity {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+  /*  @Bean
+    public UserDetailsService generateUsers() {
+        UserDetails admin = User.builder()
+                .username("root")
+                .password(passwordEncoder().encode("root"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails usuaro = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("123"))
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails swagger = User.builder()
+                .username("swagger")
+                .password(passwordEncoder().encode("123"))
+                .roles("SWAGGER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, usuaro, swagger);
+
+
+    }
+*/
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
