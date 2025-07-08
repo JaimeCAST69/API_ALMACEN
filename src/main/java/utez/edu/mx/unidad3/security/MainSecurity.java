4599928b037aa4e36f29fec69dc343cc3e128299
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-07-07 19:08:04 -0600

    [UPDATE] implementacion de jwt terminada

--- src/main/java/utez/edu/mx/unidad3/security/MainSecurity.java
+++ src/main/java/utez/edu/mx/unidad3/security/MainSecurity.java
@@ -1,83 +1,87 @@
 package utez.edu.mx.unidad3.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import utez.edu.mx.unidad3.security.filters.JWTFilter;
 
 import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 public class MainSecurity {
+    @Autowired
+    private JWTFilter jwtFilter;
+
+    private final String[] SWAGGER_URLS = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/swagger-resources/**",
+            "/webjars/**"
+    };
+
     @Bean
-    public SecurityFilterChain doFilterInternal(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable()).cors(c-> c.configurationSource(corsRegistry()))
-                .authorizeHttpRequests(auth ->auth
-                        .requestMatchers("/api/client/**").hasRole("ADMIN")
-                        .requestMatchers("/api/cedes/**").hasRole("EMPLOYEE")
-                        .requestMatchers(
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/v3/api-docs.yaml",
-                                "/swagger-resources/**",
-                                "/webjars/**"
-                        ).permitAll()
-                        .anyRequest().authenticated());
+    public SecurityFilterChain doFilterInternal(HttpSecurity http) throws Exception{
+        http.csrf(c-> c.disable()).cors(c-> c.configurationSource(corsRegistry())).authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/client/**").hasRole("ADMIN")
+                .requestMatchers("/api/cede/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
+                .requestMatchers(SWAGGER_URLS).permitAll()
+                .anyRequest().authenticated()
+        ).sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    private CorsConfigurationSource corsRegistry() {
-        //se genera la cofiguracion del cors
+    private CorsConfigurationSource corsRegistry(){
+        //Se genera toda la configuracion del CORS
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET","POST","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(false);
-    //aque rutas queremso aplicaar dihca configruacion
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        configuration.setAllowCredentials(false);//cookies
+
+        //Se especifica a que rutas queremos aplicar dicha configuracion
+        UrlBasedCorsConfigurationSource sourse = new UrlBasedCorsConfigurationSource();
+        sourse.registerCorsConfiguration("/**",configuration);
+        return sourse;
     }
 
-  /*  @Bean
-    public UserDetailsService generateUsers() {
+    /*@Bean
+    public UserDetailsService generateAdmin(){
         UserDetails admin = User.builder()
                 .username("root")
                 .password(passwordEncoder().encode("root"))
                 .roles("ADMIN")
                 .build();
-
-        UserDetails usuaro = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("123"))
+        UserDetails employee = User.builder()
+                .username("cliente")
+                .password(passwordEncoder().encode("root"))
                 .roles("EMPLOYEE")
                 .build();
-
-        UserDetails swagger = User.builder()
-                .username("swagger")
-                .password(passwordEncoder().encode("123"))
-                .roles("SWAGGER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, usuaro, swagger);
+        return new InMemoryUserDetailsManager(admin);
+    }*/
 
 
-    }
-*/
+
+
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/utez/edu/mx/unidad3/security/jwt/JWTUtils.java
+++ src/main/java/utez/edu/mx/unidad3/security/jwt/JWTUtils.java
@@ -2,38 +2,67 @@ package utez.edu.mx.unidad3.security.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JWTUtils {
     @Value("${secret.key}")
     private String SECRET_KEY;
-    //obtiene el usuario del token
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-    //obtiene la fecha de expiracion del token
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
+
+    //Obtiene el usuario del token
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
     }
 
-    //resuelve el cuerpo del token
-    //regresa el cuerpo del token resuelto
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-    //firma el token con la palabra secreta
-    //obtiene el cuerpo del token
-
-    public Claims extractAllClaims(String token) {
-      return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    //Obtiene la fecha de expiración del token
+    public Date extractExpiration(String token){
+        return extractClaims(token, Claims::getExpiration);
     }
 
+    //Resuelve el cuerpo del token y regresa el cuerpo del token resuelto
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims CLAIMS = extractAllClaims(token);
+        return claimsResolver.apply(CLAIMS);
+    }
 
+    //Firma el token con la palabra secreta y obtiene el cuerpo del token
+    public Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    //Validar que el token no este expirado
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    //Consume la funcion de arriba adicional a que pregunta que el usuario coincida con el usuario que lo usa
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String USERNAME = extractUsername(token);
+        return (USERNAME.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    //Crea el token a raiz de los datos del usuario y el token
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    //Consume la funcion de crear oara solamente exportar el token
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 horas
+                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
