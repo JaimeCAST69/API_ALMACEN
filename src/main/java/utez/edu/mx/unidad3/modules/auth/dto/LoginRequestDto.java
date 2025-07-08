93fa7b1215e31287c4229b4a23a7b983887520ca
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-07-07 19:11:35 -0600

    FIN

---
+++ src/main/java/utez/edu/mx/unidad3/modules/auth/dto/LoginRequestDto.java
@@ -0,0 +1,27 @@
+package utez.edu.mx.unidad3.modules.auth.dto;
+
+public class LoginRequestDto {
+    private String username;
+    private String password;
+
+
+    public LoginRequestDto() {
+    }
+    public LoginRequestDto(String username, String password) {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
